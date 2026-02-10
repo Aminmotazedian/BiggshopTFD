56477805bd9c85a6ca91c2f7c9bd82762ede75d6
AminMotazedian <AminMotazedian@outlook.com>
2026-02-10 17:18:57 +0330

    change readme

--- WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e44d3fff52a9fc5ce1f5c442c1f069281d3dd445")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
